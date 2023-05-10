a1748426441fadbd42c07c259c7eda8e2529b498
composercordero <composer.cordero@gmail.com>
2023-05-09 23:40:31 -0500

    Commit test, Figuring out grid
    Simple test to reconnect to git and later create a branch.

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ const defaultGrid = 16;
 for (let i = 0; i < defaultGrid; i++){
     const boxes = document.createElement('div');
     boxes.className = "box";
-    boxes.id = i+1 < 9 ? "0" + (i+1) : i+1;
+    boxes.id = i+1 <= 9 ? "0" + (i+1) : i+1;
     container.appendChild(boxes);
     let example = document.querySelectorAll(".box").textContent = `${boxes.id}`;
-    boxes.append(example);
- }
+    boxes.append(example);}
 
  function changeGridButton () {
     let answer = prompt("Enter a number between 1 and 64");
@@ -18,3 +17,27 @@ for (let i = 0; i < defaultGrid; i++){
         console.log("yes!");
     }else {console.log("try again")};
  }
+
+
+
+ //Study and implement
+
+//  function buildGrid(x, y, cellSize, gridElement) {
+//     gridElement.style.display = "grid";
+//     gridElement.style.gridTemplateColumns = `repeat(${x}, ${cellSize}px)`;
+//     gridElement.style.gridTemplateRows = `repeat(${y}, ${cellSize}px)`;
+   
+//     let squares = new DocumentFragment();
+  
+//     for (let i = 0; i < x * y; i++) {
+//       let square = document.createElement('div');
+//       square.className = 'square';
+//       squares.appendChild(square);
+//     }
+  
+//     gridElement.appendChild(squares);
+//   }
+  
+  
+//   buildGrid(15, 15, 20,  document.querySelector(".grid"));
+  
