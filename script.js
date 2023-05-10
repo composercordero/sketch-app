17325f4c81ebac9a2af9a00e8aeced437ce5d761
composercordero <composer.cordero@gmail.com>
2023-05-10 00:08:27 -0500

    Add grid
    Study example found online and implement. Now time to connect the
    user interactions with the grid (new concept).

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const container = document.querySelector('#grid');
 let output = document.getElementById("demo").textContent += " 16";
 
-const defaultGrid = 16;
+function buildGrid(x, y, cellSize, gridElement) {
+    gridElement.style.display = "grid";
+    gridElement.style.gridTemplateColumns = `repeat(${x}, ${cellSize}px)`;
+    gridElement.style.gridTemplateRows = `repeat(${y}, ${cellSize}px)`;
+   
+    let container = new DocumentFragment();
+  
+    for (let i = 0; i < x * y; i++) {
+      let box = document.createElement('div');
+      box.className = 'box';
+      container.appendChild(box);
+    }
 
-for (let i = 0; i < defaultGrid; i++){
-    const boxes = document.createElement('div');
-    boxes.className = "box";
-    boxes.id = i+1 < 9 ? "0" + (i+1) : i+1;
-    container.appendChild(boxes);
-    let example = document.querySelectorAll(".box").textContent = `${boxes.id}`;
-    boxes.append(example);
- }
+    gridElement.appendChild(container);
+
+  }
+
+  buildGrid(20, 20, 25,  document.querySelector("#grid"));
+  
 
  function changeGridButton () {
     let answer = prompt("Enter a number between 1 and 64");
@@ -18,3 +26,14 @@ for (let i = 0; i < defaultGrid; i++){
         console.log("yes!");
     }else {console.log("try again")};
  }
+
+
+
+ // for (let i = 0; i < defaultGrid; i++){
+//     const boxes = document.createElement('div');
+//     boxes.className = "box";
+//     boxes.id = i+1 < 9 ? "0" + (i+1) : i+1;
+//     container.appendChild(boxes);
+//     let example = document.querySelectorAll(".box").textContent = `${boxes.id}`;
+//     boxes.append(example);
+//  }
