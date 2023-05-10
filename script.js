099dd7a7ef34114c97539487c2bf79b51063187e
composercordero <composer.cordero@gmail.com>
2023-05-10 16:05:54 -0500

    Study and implement reload grid
    Study example found online and apply to project. Next is buttons.

--- script.js
+++ script.js
@@ -1,34 +1,77 @@
+//Variables
 let output = document.getElementById("demo").textContent += " 16";
+// let setCurrentMode = defaultMode;
 
-function buildGrid(x, y, cellSize, gridElement) {
-    gridElement.style.display = "grid";
-    gridElement.style.gridTemplateColumns = `repeat(${x}, ${cellSize}px)`;
-    gridElement.style.gridTemplateRows = `repeat(${y}, ${cellSize}px)`;
+//Buttons
+const colorBtn = document.getElementById('color')
+const rainbowBtn = document.getElementById('random-color')
+const eraserBtn = document.getElementById('eraser')
+const clearBtn = document.getElementById('clear')
+
+//Constants
+const grid = document.getElementById('grid')
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 20;
+
+//Event Listeners
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('random')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+clearBtn.onclick = () => reloadGrid()
+const changeBtn = document.getElementById("changeGrid");
+changeBtn.addEventListener("click", changeGridButton);
+
+//Functions
+function setCurrentSize(newSize) {
+    currentSize = newSize
+  }
+
+function changeSize(value) {
+setCurrentSize(value);
+output = document.getElementById("demo").textContent += `${value}`;
+reloadGrid();
+}
+
+function updateSizeValue(value) {
+sizeValue.innerHTML = `${value} x ${value}`
+}
+
+function reloadGrid() {
+clearGrid()
+setupGrid(currentSize)
+}
+
+function clearGrid() {
+grid.innerHTML = ''
+}
+
+function setupGrid(size) {
+    grid.style.display = "grid";
+    grid.style.gridTemplateColumns = `repeat(${size}, 20px)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 20px)`;
    
     let container = new DocumentFragment();
   
-    for (let i = 0; i < x * y; i++) {
-      let box = document.createElement('div');
-      box.className = 'box';
-      container.appendChild(box);
+    for (let i = 0; i < size * size; i++) {
+        let box = document.createElement('div');
+        box.className = 'box';
+        //   gridElement.addEventListener('mouseover', changeColor)
+        //   gridElement.addEventListener('mousedown', changeColor)
+      grid.appendChild(box);
     }
-
-    gridElement.appendChild(container);
-
-  }
-
-  buildGrid(20, 20, 25,  document.querySelector("#grid"));
-  
+}
 
  function changeGridButton () {
-    let answer = prompt("Enter a number between 1 and 64");
-    if (answer => 1 || answer <= 64){
-        console.log("yes!");
-    }else {console.log("try again")};
+    let answer = prompt("Enter a number between 16 and 48");
+    if (answer >= 16 && answer <= 48){
+        changeSize(answer);
+    }else {alert("try again")};
  }
 
+ //Window Load
+setupGrid(DEFAULT_SIZE);
 
-
+//My previous code
  // for (let i = 0; i < defaultGrid; i++){
 //     const boxes = document.createElement('div');
 //     boxes.className = "box";
